d6634804c2e54b4c98fc604c5e0ab5e2d11e586d
Elder Estrada <elder.s.estrada@gmail.com>
2026-09-14 16:37:09 +0200

    Agregar archivos base y dependencias para Mini-Nginx

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+mod config;
+mod connection;
+mod error;
+mod http;
+mod logging;
+mod router;
+mod server;
+mod static_files;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Mini-Nginx: inicialzando estructura base...")
 }
